1a278be6f5897c7bb3e5d491d92ea5ae2990f82d
Gergely Brautigam <skarlso777@gmail.com>
2018-05-29 07:54:18 +0200

    Getting jobs.

--- server/main.go
+++ server/main.go
@@ -6,11 +6,15 @@ import (
 	"log"
 	"net/http"
 	"path/filepath"
+	"strconv"
 	"time"
 
 	"github.com/gorilla/mux"
 	"gopkg.in/yaml.v2"
 )
+
+// TODO: Need a way to persist this count
+var jobCount int
 
 // PostJob accepts a yaml file to parse which contains steps to do.
 func PostJob(w http.ResponseWriter, r *http.Request) {
@@ -32,7 +36,21 @@ func PostJob(w http.ResponseWriter, r *http.Request) {
 	}
 	j.Parse()
 	// server.SendToNoneBusyWorker(j.Translated)
+	// CREATE a Job! The job will have an agent assigned
+	// As long as the agent is assigned to a Job it's busy.
+	// And the job will take care of the commands that it needs to execute on that agent.
+
 	server.executeViaSSH(j.Translated)
+}
+
+// GetJob will attach to the log output of the job with number ID.
+func GetJob(w http.ResponseWriter, r *http.Request) {
+	v := r.URL.Query()
+	id, err := strconv.Atoi(v.Get("id"))
+	if err != nil {
+		fmt.Fprintf(w, "cannot convert to number: '%s'", id)
+	}
+	fmt.Fprintf(w, "looking up job number: '%d'", id)
 }
 
 func loadPlugins() {
@@ -65,5 +83,6 @@ func main() {
 
 	router := mux.NewRouter()
 	router.HandleFunc("/jobs/add", PostJob).Methods("POST")
+	router.HandleFunc("/jobs/{id:[0-9]+}", GetJob).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
